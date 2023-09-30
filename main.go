504e5d666a7955a388e560aee07699b7d6f50425
miyakiyu <sherry910509@gmail.com>
2023-09-30 16:36:33 +0800

    Just a small adjust

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"os/exec"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,29 +10,18 @@ func main() {
 	r := gin.Default()        //start the server
 	r.LoadHTMLGlob("pages/*") //Load all file in the pages folder
 
-	r.GET("/hmm", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main.html", gin.H{
 			"title": "here is my home now",
 		})
 	})
 
-	r.GET("/", func(c *gin.Context) {
-		//execute program
-		cmd := exec.Command("./add")
-		//get the output and error
-		out, err := cmd.Output()
-		//error handler
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error executing C program")
-			return
-		}
-		//show result
-		result := string(out)
-		c.String(http.StatusOK, "Result: %s", result)
-	})
-
 	r.GET("/status", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "status.html", gin.H{})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "ping.html", gin.H{})
 	})
 
 	//Gogo
